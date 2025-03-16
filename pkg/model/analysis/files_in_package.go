9406762e811a4310e7804a4b597906c2689883d7
Ed Schouten <eschouten@apple.com>
2025-03-16 17:23:14 +0100

    Make FilesInPackage a bit more complete
    Provide a basic CapturableDirectory implementation. Right now it's still
    missing ReadDir() though, so it's fairly broken.

--- pkg/model/analysis/files_in_package.go
+++ pkg/model/analysis/files_in_package.go
@@ -3,15 +3,101 @@ package analysis
 import (
 	"context"
 	"errors"
+	"sort"
+	"strings"
 
+	"github.com/buildbarn/bb-storage/pkg/filesystem"
+	"github.com/buildbarn/bb-storage/pkg/filesystem/path"
 	"github.com/buildbarn/bonanza/pkg/evaluation"
 	model_core "github.com/buildbarn/bonanza/pkg/model/core"
 	model_filesystem "github.com/buildbarn/bonanza/pkg/model/filesystem"
 	model_analysis_pb "github.com/buildbarn/bonanza/pkg/proto/model/analysis"
+	model_filesystem_pb "github.com/buildbarn/bonanza/pkg/proto/model/filesystem"
 
 	"golang.org/x/sync/errgroup"
 	"golang.org/x/sync/semaphore"
 )
+
+type currentPackageLimitingDirectoryOptions[TReference any] struct {
+	context          context.Context
+	directoryReaders *DirectoryReaders[TReference]
+}
+
+// currentPackageLimitingDirectory is an implementation of
+// CapturableDirectory that suppresses child directories that are not
+// part of the current package. Furthermore, it strips the contents of
+// all files. This leaves the only data that is necessary for making
+// features like glob() work.
+type currentPackageLimitingDirectory[TReference any, TDirectory, TFile model_core.ReferenceMetadata] struct {
+	options   *currentPackageLimitingDirectoryOptions[TReference]
+	directory model_core.Message[*model_filesystem_pb.Directory, TReference]
+}
+
+func (d *currentPackageLimitingDirectory[TReference, TDirectory, TFile]) Close() error {
+	*d = currentPackageLimitingDirectory[TReference, TDirectory, TFile]{}
+	return nil
+}
+
+func (currentPackageLimitingDirectory[TReference, TDirectory, TFile]) ReadDir() ([]filesystem.FileInfo, error) {
+	return nil, errors.New("TODO: Implement ReadDir()")
+}
+
+func (d *currentPackageLimitingDirectory[TReference, TDirectory, TFile]) Readlink(name path.Component) (path.Parser, error) {
+	leaves, err := model_filesystem.DirectoryGetLeaves(
+		d.options.context,
+		d.options.directoryReaders.Leaves,
+		d.directory,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	symlinks := leaves.Message.Symlinks
+	nameStr := name.String()
+	index, ok := sort.Find(
+		len(symlinks),
+		func(i int) int { return strings.Compare(nameStr, symlinks[i].Name) },
+	)
+	if !ok {
+		panic("attempted to read a symbolic link that was not reported by ReadDir()")
+	}
+	return path.UNIXFormat.NewParser(symlinks[index].Target), nil
+}
+
+func (d *currentPackageLimitingDirectory[TReference, TDirectory, TFile]) EnterCapturableDirectory(name path.Component) (*model_filesystem.CreatedDirectory[TDirectory], model_filesystem.CapturableDirectory[TDirectory, TFile], error) {
+	directories := d.directory.Message.Directories
+	nameStr := name.String()
+	index, ok := sort.Find(
+		len(directories),
+		func(i int) int { return strings.Compare(nameStr, directories[i].Name) },
+	)
+	if !ok {
+		panic("attempted to enter a directory that was not reported by ReadDir()")
+	}
+	childDirectory, err := model_filesystem.DirectoryNodeGetContents(
+		d.options.context,
+		d.options.directoryReaders.Directory,
+		model_core.NewNestedMessage(d.directory, directories[index]),
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+	return nil, &currentPackageLimitingDirectory[TReference, TDirectory, TFile]{
+		options:   d.options,
+		directory: childDirectory,
+	}, nil
+}
+
+func (currentPackageLimitingDirectory[TReference, TDirectory, TFile]) OpenForFileMerkleTreeCreation(name path.Component) (model_filesystem.CapturableFile[TFile], error) {
+	return emptyCapturableFile[TFile]{}, nil
+}
+
+type emptyCapturableFile[TMetadata model_core.ReferenceMetadata] struct{}
+
+func (emptyCapturableFile[TMetadata]) CreateFileMerkleTree(ctx context.Context) (model_core.PatchedMessage[*model_filesystem_pb.FileContents, TMetadata], error) {
+	return model_core.NewSimplePatchedMessage[TMetadata]((*model_filesystem_pb.FileContents)(nil)), nil
+}
+func (emptyCapturableFile[TMetadata]) Discard() {}
 
 func (c *baseComputer[TReference, TMetadata]) ComputeFilesInPackageValue(ctx context.Context, key *model_analysis_pb.FilesInPackage_Key, e FilesInPackageEnvironment[TReference, TMetadata]) (PatchedFilesInPackageValue, error) {
 	directoryCreationParameters, gotDirectoryCreationParameters := e.GetDirectoryCreationParametersObjectValue(&model_analysis_pb.DirectoryCreationParametersObject_Key{})
@@ -36,7 +122,13 @@ func (c *baseComputer[TReference, TMetadata]) ComputeFilesInPackageValue(ctx con
 			semaphore.NewWeighted(1),
 			group,
 			directoryCreationParameters,
-			nil, // TODO: Directory!
+			&currentPackageLimitingDirectory[TReference, TMetadata, TMetadata]{
+				options: &currentPackageLimitingDirectoryOptions[TReference]{
+					context:          ctx,
+					directoryReaders: directoryReaders,
+				},
+				directory: packageDirectory,
+			},
 			nil, // TODO: Capturer!
 			&trimmedPackageDirectory,
 		)
