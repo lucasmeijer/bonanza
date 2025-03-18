cd343f2c94053d06d2ac42e1222438a96ffdeb41
Ed Schouten <eschouten@apple.com>
2025-03-18 09:17:46 -0700

    Add some basic testing coverage for globs containing **

--- pkg/glob/matcher_test.go
+++ pkg/glob/matcher_test.go
@@ -52,4 +52,52 @@ func TestMatcher(t *testing.T) {
 			require.False(t, m.WriteRune('/'))
 		})
 	})
+
+	t.Run("StarStarSlash", func(t *testing.T) {
+		nfa, err := glob.NewNFAFromPatterns(
+			[]string{
+				"**/foo.h",
+			},
+			nil,
+		)
+		require.NoError(t, err)
+
+		t.Run("AtRoot", func(t *testing.T) {
+			var m glob.Matcher
+			m.Initialize(nfa)
+			require.True(t, m.WriteRune('f'))
+			require.False(t, m.IsMatch())
+			require.True(t, m.WriteRune('o'))
+			require.False(t, m.IsMatch())
+			require.True(t, m.WriteRune('o'))
+			require.False(t, m.IsMatch())
+			require.True(t, m.WriteRune('.'))
+			require.False(t, m.IsMatch())
+			require.True(t, m.WriteRune('h'))
+			require.True(t, m.IsMatch())
+		})
+
+		t.Run("InDirectory", func(t *testing.T) {
+			var m glob.Matcher
+			m.Initialize(nfa)
+			require.True(t, m.WriteRune('b'))
+			require.False(t, m.IsMatch())
+			require.True(t, m.WriteRune('a'))
+			require.False(t, m.IsMatch())
+			require.True(t, m.WriteRune('r'))
+			require.False(t, m.IsMatch())
+			require.True(t, m.WriteRune('/'))
+			require.False(t, m.IsMatch())
+			require.True(t, m.WriteRune('f'))
+			require.False(t, m.IsMatch())
+			require.True(t, m.WriteRune('o'))
+			require.False(t, m.IsMatch())
+			require.True(t, m.WriteRune('o'))
+			require.False(t, m.IsMatch())
+			require.True(t, m.WriteRune('.'))
+			require.False(t, m.IsMatch())
+			require.True(t, m.WriteRune('h'))
+			require.True(t, m.IsMatch())
+		})
+	})
 }
