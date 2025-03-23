f13905a43658149b2d189e7f9a93ff6485341e1c
Ed Schouten <eschouten@apple.com>
2025-03-22 19:51:48 -0700

    Let CompatibleToolchainsForType call VisibleTarget on the platform
    Right now we have some code to manually attempt to extract the target
    platform the configuration. It turns out we don't need to do that if we
    simply call VisibleTarget. VisibleTarget is already capable of expanding
    label_flag()s. The advantage is that we get less repetition of code, and
    also allow target platforms to be aliases.

--- pkg/model/analysis/compatible_toolchains_for_type.go
+++ pkg/model/analysis/compatible_toolchains_for_type.go
@@ -2,19 +2,15 @@ package analysis
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"strings"
 
 	"github.com/buildbarn/bonanza/pkg/evaluation"
 	"github.com/buildbarn/bonanza/pkg/label"
 	model_core "github.com/buildbarn/bonanza/pkg/model/core"
-	"github.com/buildbarn/bonanza/pkg/model/core/btree"
 	model_parser "github.com/buildbarn/bonanza/pkg/model/parser"
 	model_starlark "github.com/buildbarn/bonanza/pkg/model/starlark"
 	model_analysis_pb "github.com/buildbarn/bonanza/pkg/proto/model/analysis"
 	model_core_pb "github.com/buildbarn/bonanza/pkg/proto/model/core"
-	model_starlark_pb "github.com/buildbarn/bonanza/pkg/proto/model/starlark"
 	"github.com/buildbarn/bonanza/pkg/storage/dag"
 )
 
@@ -29,68 +25,30 @@ func (c *baseComputer[TReference, TMetadata]) getConfigurationByReference(ctx co
 var commandLineOptionPlatformsLabel = label.MustNewCanonicalLabel("@@bazel_tools+//command_line_option:platforms")
 
 func (c *baseComputer[TReference, TMetadata]) ComputeCompatibleToolchainsForTypeValue(ctx context.Context, key model_core.Message[*model_analysis_pb.CompatibleToolchainsForType_Key, TReference], e CompatibleToolchainsForTypeEnvironment[TReference, TMetadata]) (PatchedCompatibleToolchainsForTypeValue, error) {
+	patchedConfigurationReference := model_core.NewPatchedMessageFromExistingCaptured(
+		e,
+		model_core.NewNestedMessage(key, key.Message.ConfigurationReference),
+	)
+	platformLabel := e.GetVisibleTargetValue(
+		model_core.NewPatchedMessage(
+			&model_analysis_pb.VisibleTarget_Key{
+				ConfigurationReference: patchedConfigurationReference.Message,
+				FromPackage:            commandLineOptionPlatformsLabel.GetCanonicalPackage().String(),
+				ToLabel:                commandLineOptionPlatformsLabel.String(),
+			},
+			model_core.MapReferenceMetadataToWalkers(patchedConfigurationReference.Patcher),
+		),
+	)
 	registeredToolchains := e.GetRegisteredToolchainsForTypeValue(&model_analysis_pb.RegisteredToolchainsForType_Key{
 		ToolchainType: key.Message.ToolchainType,
 	})
-	if !registeredToolchains.IsSet() {
+	if !platformLabel.IsSet() || !registeredToolchains.IsSet() {
 		return PatchedCompatibleToolchainsForTypeValue{}, evaluation.ErrMissingDependency
 	}
 
-	// Obtain the constraints associated with the current platform.
-	configuration, err := c.getConfigurationByReference(
-		ctx,
-		model_core.NewNestedMessage(key, key.Message.ConfigurationReference),
-	)
-	if err != nil {
-		return PatchedCompatibleToolchainsForTypeValue{}, err
-	}
-	commandLineOptionPlatformsLabelStr := commandLineOptionPlatformsLabel.String()
-	platformOverride, err := btree.Find(
-		ctx,
-		c.configurationBuildSettingOverrideReader,
-		model_core.NewNestedMessage(configuration, configuration.Message.BuildSettingOverrides),
-		func(entry *model_analysis_pb.Configuration_BuildSettingOverride) (int, *model_core_pb.Reference) {
-			switch level := entry.Level.(type) {
-			case *model_analysis_pb.Configuration_BuildSettingOverride_Leaf_:
-				return strings.Compare(commandLineOptionPlatformsLabelStr, level.Leaf.Label), nil
-			case *model_analysis_pb.Configuration_BuildSettingOverride_Parent_:
-				return strings.Compare(commandLineOptionPlatformsLabelStr, level.Parent.FirstLabel), level.Parent.Reference
-			default:
-				return 0, nil
-			}
-		},
-	)
-	if err != nil {
-		return PatchedCompatibleToolchainsForTypeValue{}, err
-	}
-	var platformLabel string
-	if platformOverride.IsSet() {
-		leaf, ok := platformOverride.Message.Level.(*model_analysis_pb.Configuration_BuildSettingOverride_Leaf_)
-		if !ok {
-			return PatchedCompatibleToolchainsForTypeValue{}, errors.New("build setting override is not a valid leaf")
-		}
-		labelValue, ok := leaf.Leaf.Value.GetKind().(*model_starlark_pb.Value_Label)
-		if !ok {
-			return PatchedCompatibleToolchainsForTypeValue{}, fmt.Errorf("build setting override for %#v is not a label", commandLineOptionPlatformsLabelStr)
-		}
-		platformLabel = labelValue.Label
-	} else {
-		targetValue := e.GetTargetValue(&model_analysis_pb.Target_Key{
-			Label: commandLineOptionPlatformsLabelStr,
-		})
-		if !targetValue.IsSet() {
-			return PatchedCompatibleToolchainsForTypeValue{}, evaluation.ErrMissingDependency
-		}
-		labelSetting, ok := targetValue.Message.Definition.GetKind().(*model_starlark_pb.Target_Definition_LabelSetting)
-		if !ok {
-			return PatchedCompatibleToolchainsForTypeValue{}, fmt.Errorf("target %#v is not a label setting", commandLineOptionPlatformsLabelStr)
-		}
-		platformLabel = labelSetting.LabelSetting.BuildSettingDefault
-	}
-	// TODO: VisibleTarget?
 	platformInfoProvider, err := getProviderFromConfiguredTarget(
 		e,
-		platformLabel,
+		platformLabel.Message.Label,
 		model_core.NewSimplePatchedMessage[model_core.WalkableReferenceMetadata, *model_core_pb.Reference](nil),
 		platformInfoProviderIdentifier,
 	)
