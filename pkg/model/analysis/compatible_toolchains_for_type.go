f13bd4ce0fd1cc9a3a14df1a426fd1b653337d22
Ed Schouten <eschouten@apple.com>
2025-03-23 21:30:39 -0700

    Factor out getting PlatformInfo for target platform
    We can also use a function like this to implement
    ctx.target_platform_has_constraint().

--- pkg/model/analysis/compatible_toolchains_for_type.go
+++ pkg/model/analysis/compatible_toolchains_for_type.go
@@ -11,7 +11,9 @@ import (
 	model_starlark "github.com/buildbarn/bonanza/pkg/model/starlark"
 	model_analysis_pb "github.com/buildbarn/bonanza/pkg/proto/model/analysis"
 	model_core_pb "github.com/buildbarn/bonanza/pkg/proto/model/core"
+	model_starlark_pb "github.com/buildbarn/bonanza/pkg/proto/model/starlark"
 	"github.com/buildbarn/bonanza/pkg/storage/dag"
+	"github.com/buildbarn/bonanza/pkg/storage/object"
 )
 
 func (c *baseComputer[TReference, TMetadata]) getConfigurationByReference(ctx context.Context, configurationReference model_core.Message[*model_core_pb.Reference, TReference]) (model_core.Message[*model_analysis_pb.Configuration, TReference], error) {
@@ -24,11 +26,18 @@ func (c *baseComputer[TReference, TMetadata]) getConfigurationByReference(ctx co
 
 var commandLineOptionPlatformsLabel = label.MustNewCanonicalLabel("@@bazel_tools+//command_line_option:platforms")
 
-func (c *baseComputer[TReference, TMetadata]) ComputeCompatibleToolchainsForTypeValue(ctx context.Context, key model_core.Message[*model_analysis_pb.CompatibleToolchainsForType_Key, TReference], e CompatibleToolchainsForTypeEnvironment[TReference, TMetadata]) (PatchedCompatibleToolchainsForTypeValue, error) {
-	patchedConfigurationReference := model_core.NewPatchedMessageFromExistingCaptured(
-		e,
-		model_core.NewNestedMessage(key, key.Message.ConfigurationReference),
-	)
+type getTargetPlatformInfoProviderEnvironment[TReference any, TMetadata any] interface {
+	model_core.ExistingObjectCapturer[TReference, TMetadata]
+	getProviderFromConfiguredTargetEnvironment[TReference]
+
+	GetVisibleTargetValue(model_core.PatchedMessage[*model_analysis_pb.VisibleTarget_Key, dag.ObjectContentsWalker]) model_core.Message[*model_analysis_pb.VisibleTarget_Value, TReference]
+}
+
+func getTargetPlatformInfoProvider[TReference object.BasicReference, TMetadata BaseComputerReferenceMetadata](
+	e getTargetPlatformInfoProviderEnvironment[TReference, TMetadata],
+	configurationReference model_core.Message[*model_core_pb.Reference, TReference],
+) (model_core.Message[*model_starlark_pb.Struct_Fields, TReference], error) {
+	patchedConfigurationReference := model_core.NewPatchedMessageFromExistingCaptured(e, configurationReference)
 	platformLabel := e.GetVisibleTargetValue(
 		model_core.NewPatchedMessage(
 			&model_analysis_pb.VisibleTarget_Key{
@@ -39,11 +48,8 @@ func (c *baseComputer[TReference, TMetadata]) ComputeCompatibleToolchainsForType
 			model_core.MapReferenceMetadataToWalkers(patchedConfigurationReference.Patcher),
 		),
 	)
-	registeredToolchains := e.GetRegisteredToolchainsForTypeValue(&model_analysis_pb.RegisteredToolchainsForType_Key{
-		ToolchainType: key.Message.ToolchainType,
-	})
-	if !platformLabel.IsSet() || !registeredToolchains.IsSet() {
-		return PatchedCompatibleToolchainsForTypeValue{}, evaluation.ErrMissingDependency
+	if !platformLabel.IsSet() {
+		return model_core.Message[*model_starlark_pb.Struct_Fields, TReference]{}, evaluation.ErrMissingDependency
 	}
 
 	platformInfoProvider, err := getProviderFromConfiguredTarget(
@@ -53,15 +59,31 @@ func (c *baseComputer[TReference, TMetadata]) ComputeCompatibleToolchainsForType
 		platformInfoProviderIdentifier,
 	)
 	if err != nil {
-		return PatchedCompatibleToolchainsForTypeValue{}, fmt.Errorf("failed to obtain PlatformInfo provider for target %#v: %w", platformLabel, err)
+		return model_core.Message[*model_starlark_pb.Struct_Fields, TReference]{}, fmt.Errorf("failed to obtain PlatformInfo provider for target %#v: %w", platformLabel, err)
 	}
+	return platformInfoProvider, nil
+}
+
+func (c *baseComputer[TReference, TMetadata]) ComputeCompatibleToolchainsForTypeValue(ctx context.Context, key model_core.Message[*model_analysis_pb.CompatibleToolchainsForType_Key, TReference], e CompatibleToolchainsForTypeEnvironment[TReference, TMetadata]) (PatchedCompatibleToolchainsForTypeValue, error) {
+	registeredToolchains := e.GetRegisteredToolchainsForTypeValue(&model_analysis_pb.RegisteredToolchainsForType_Key{
+		ToolchainType: key.Message.ToolchainType,
+	})
+	if !registeredToolchains.IsSet() {
+		return PatchedCompatibleToolchainsForTypeValue{}, evaluation.ErrMissingDependency
+	}
+
+	platformInfoProvider, err := getTargetPlatformInfoProvider(e, model_core.NewNestedMessage(key, key.Message.ConfigurationReference))
+	if err != nil {
+		return PatchedCompatibleToolchainsForTypeValue{}, err
+	}
+
 	constraintsValue, err := model_starlark.GetStructFieldValue(ctx, c.valueReaders.List, platformInfoProvider, "constraints")
 	if err != nil {
-		return PatchedCompatibleToolchainsForTypeValue{}, fmt.Errorf("failed to obtain constraints field of PlatformInfo provider for target %#v: %w", err)
+		return PatchedCompatibleToolchainsForTypeValue{}, fmt.Errorf("failed to obtain constraints field of PlatformInfo provider of target platform: %w", err)
 	}
 	constraints, err := c.extractFromPlatformInfoConstraints(ctx, constraintsValue)
 	if err != nil {
-		return PatchedCompatibleToolchainsForTypeValue{}, fmt.Errorf("failed to extract constraints from PlatformInfo provider for target %#v", platformLabel)
+		return PatchedCompatibleToolchainsForTypeValue{}, fmt.Errorf("failed to extract constraints from PlatformInfo provider of target platform: %w", err)
 	}
 
 	allToolchains := registeredToolchains.Message.Toolchains
