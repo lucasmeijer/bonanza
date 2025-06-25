b6e8df5d8231cb693ef4f066b6ae7755f5a171db
Ed Schouten <eschouten@apple.com>
2025-06-25 13:35:00 +0200

    Improve documentation of FlatReference

--- pkg/storage/object/flat_reference.go
+++ pkg/storage/object/flat_reference.go
@@ -6,9 +6,16 @@ import (
 )
 
 // FlatReference is a simplified version of LocalReference that assumes
-// that the height and degree of an object is zero. This is sufficient
-// for the read caching backend, which is oblivious of references
-// between objects.
+// that the height and degree of an object are zero. This means that
+// only the hash and size need to be tracked, similar to REv2's Digest
+// message.
+//
+// Because the hash and size are stored at the beginning of a
+// LocalReference, FlatReference uses the same representation as
+// LocalReference with the last couple of bytes removed.
+//
+// FlatReference is sufficient for the read caching and local storage
+// backends, which are oblivious of references between objects.
 type FlatReference struct {
 	rawReference [35]byte
 }
