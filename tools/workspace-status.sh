2d1d422bf6a249fc8632ce13656eed65dcb74782
Lucas Meijer <lucas@lucasmeijer.com>
2026-01-14 13:15:25 +0100

    make workspace-status.sh macos compatible + not tied to github actions

--- tools/workspace-status.sh
+++ tools/workspace-status.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-if test "${GITHUB_ACTIONS}" = "true"; then
+if test "${GITHUB_ACTIONS}" = "true" || test "${BONANZA_ENABLE_STAMP}" = "true"; then
   echo "BUILD_SCM_REVISION $(git rev-parse --short HEAD)"
-  echo "BUILD_SCM_TIMESTAMP $(TZ=UTC date --date "@$(git show -s --format=%ct HEAD)" +%Y%m%dT%H%M%SZ)"
+  echo "BUILD_SCM_TIMESTAMP $(TZ=UTC git show -s --format=%cd --date=format:%Y%m%dT%H%M%SZ HEAD)"
 fi
